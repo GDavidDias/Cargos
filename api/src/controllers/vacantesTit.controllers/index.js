f459052e58014500476aa29e2b86b469609825af
GDavidDias <guillermo.david.dias@gmail.com>
2024-09-19 13:06:30 -0300

    En modal Ver Vacante, se agregan datos de Inscripto que tomo la vacante, se da funcinalidad de Modificar una Vacante no asignada

--- api/src/controllers/vacantesTit.controllers/index.js
+++ api/src/controllers/vacantesTit.controllers/index.js
@@ -1,7 +1,11 @@
 const getAllVacantesTit = require('./getAllVacantesTit.js');
 const getVacanteAsignadaTit = require('./getVacanteAsignadaTit.js');
+const getVacanteTitAsignadaInscripto = require('./getVacanteTitAsignadaInscripto.js');
+const editVacanteTit = require('./editVacanteTit.js');
 
 module.exports = {
     getAllVacantesTit,
-    getVacanteAsignadaTit
+    getVacanteAsignadaTit,
+    getVacanteTitAsignadaInscripto,
+    editVacanteTit
 };
--- api/src/routes/vacantesTit.routes.js
+++ api/src/routes/vacantesTit.routes.js
@@ -2,7 +2,9 @@ const {Router} = require('express');
 
 const {
     getAllVacantesTit,
-    getVacanteAsignadaTit
+    getVacanteAsignadaTit,
+    getVacanteTitAsignadaInscripto,
+    editVacanteTit
 }= require('../controllers/vacantesTit.controllers');
 
 const router = Router();
@@ -13,5 +15,10 @@ router.post('/allvacantestit', getAllVacantesTit);
 //tree datos de una vacante
 router.post('/vacantetit/:idVacanteTit', getVacanteAsignadaTit);
 
+//trae datos del inscripto que tiene una vacante asignada
+router.post('/vacantetitasignadainscripto/:idVacanteTit', getVacanteTitAsignadaInscripto);
+
+//edita una vacante
+router.put('/editvacantetit/:idVacanteTit',editVacanteTit);
 
 module.exports = router;
