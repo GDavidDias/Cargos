12aa0da2bcd95f4ee7c46b51c0cd88b64f6db363
GDavidDias <guillermo.david.dias@gmail.com>
2025-02-18 18:39:26 -0300

    tabla escuela

--- client/varGlobal.js
+++ client/varGlobal.js
@@ -1,5 +1,5 @@
-//const URL = 'http://localhost:3000';
-const URL = 'https://cargos.salaprimariajpcd.net.ar';
+const URL = 'http://localhost:3000';
+//const URL = 'https://cargos.salaprimariajpcd.net.ar';
 
 //const URL = 'https://cargos.gddhosting.net.ar';
 //const URL = 'https://cargosinicial.salaprimariajpcd.net.ar';
